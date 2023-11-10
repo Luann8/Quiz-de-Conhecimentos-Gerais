53f0c69dc3421d278b4925a44484ec82b168d868
Luann Pereira Mendes <133384636+Luann8@users.noreply.github.com>
2023-11-10 12:10:16 -0300

    Add files via upload

---
+++ src/data/questions_complete.js
@@ -0,0 +1,109 @@
+const data = [
+  {
+    category: "Geografia",
+    questions: [
+      {
+        question: "Qual é o maior país do mundo em termos de área terrestre?",
+        options: ["Rússia", "China", "Canadá", "Estados Unidos"],
+        answer: "Rússia",
+        tip: "É o país mais extenso, cobrindo mais de 17 milhões de quilômetros quadrados.",
+      },
+      {
+        question: "Qual é a capital da Austrália?",
+        options: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
+        answer: "Canberra",
+        tip: "Canberra foi escolhida como a capital em 1908.",
+      },
+      {
+        question: "Qual é o rio mais longo do mundo?",
+        options: ["Nilo", "Amazonas", "Yangtzé", "Mississipi"],
+        answer: "Nilo",
+        tip: "O rio Nilo é o mais longo, estendendo-se por aproximadamente 6.650 km.",
+      },
+      {
+        question: "Em que continente fica a Antártida?",
+        options: ["África", "Ásia", "Europa", "Antártida"],
+        answer: "Antártida",
+        tip: "A Antártida é um continente coberto por gelo na extremidade sul da Terra.",
+      },
+      {
+        question: "Qual o maior deserto do mundo?",
+        options: ["Deserto do Saara", "Deserto de Gobi", "Deserto da Arábia", "Deserto de Atacama"],
+        answer: "Deserto do Saara",
+        tip: "O Deserto do Saara, o Deserto da Arábia e o Deserto de Gobi são os três maiores desertos.",
+      },
+    ],
+  },
+  {
+    category: "Ciências",
+    questions: [
+      {
+        question: "Qual é o elemento mais abundante na crosta terrestre?",
+        options: ["Oxigênio", "Silício", "Alumínio", "Ferro"],
+        answer: "Oxigênio",
+        tip: "O oxigênio compõe cerca de 46,6% da crosta terrestre.",
+      },
+      {
+        question: "Quem desenvolveu a teoria da relatividade?",
+        options: ["Isaac Newton", "Albert Einstein", "Galileu Galilei", "Niels Bohr"],
+        answer: "Albert Einstein",
+        tip: "A teoria da relatividade foi publicada por Einstein em 1915.",
+      },
+      {
+        question: "Qual é o maior órgão do corpo humano?",
+        options: ["Coração", "Fígado", "Pele", "Pulmões"],
+        answer: "Pele",
+        tip: "A pele é o maior órgão do corpo humano, sendo também o órgão mais pesado.",
+      },
+      {
+        question: "O que é a fotossíntese?",
+        options: ["Respiração celular", "Digestão", "Produção de energia solar", "Reprodução assexuada"],
+        answer: "Produção de energia solar",
+        tip: "A fotossíntese é o processo pelo qual as plantas convertem a luz solar em energia.",
+      },
+      {
+        question: "Qual é o ácido presente no suco gástrico?",
+        options: ["Ácido cítrico", "Ácido acético", "Ácido clorídrico", "Ácido sulfúrico"],
+        answer: "Ácido clorídrico",
+        tip: "O ácido clorídrico é produzido pelo estômago para auxiliar na digestão dos alimentos.",
+      },
+    ],
+  },
+  {
+    category: "Arte e Cultura",
+    questions: [
+      {
+        question: "Quem pintou 'A Noite Estrelada'?",
+        options: ["Vincent van Gogh", "Pablo Picasso", "Claude Monet", "Leonardo da Vinci"],
+        answer: "Vincent van Gogh",
+        tip: "Uma das obras mais conhecidas do impressionismo.",
+      },
+      {
+        question: "Qual é a peça mais famosa de William Shakespeare?",
+        options: ["Hamlet", "Romeu e Julieta", "Macbeth", "Otelo"],
+        answer: "Hamlet",
+        tip: "É uma tragédia escrita por Shakespeare por volta de 1599-1601.",
+      },
+      {
+        question: "Quem escreveu 'Cem Anos de Solidão'?",
+        options: ["Gabriel García Márquez", "Isabel Allende", "Julio Cortázar", "Pablo Neruda"],
+        answer: "Gabriel García Márquez",
+        tip: "Um dos romances mais importantes da literatura latino-americana.",
+      },
+      {
+        question: "Qual é a dança tradicional do Havaí?",
+        options: ["Hula", "Flamenco", "Salsa", "Tango"],
+        answer: "Hula",
+        tip: "A Hula é uma dança polinésia tradicional do Havaí.",
+      },
+      {
+        question: "Quem é o autor da Mona Lisa?",
+        options: ["Leonardo da Vinci", "Michelangelo", "Rafael", "Donatello"],
+        answer: "Leonardo da Vinci",
+        tip: "A Mona Lisa é uma das obras mais famosas de da Vinci.",
+      },
+    ],
+  },
+];
+
+export default data;
